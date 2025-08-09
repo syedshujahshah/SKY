307b74d6ff3771ff128a61a7402c6888dae8773f
Syed Shujah TikTokWala <syedshujahshah7@gmail.com>
2025-08-09 05:28:58 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$host = 'localhost';
+$dbname = 'dbcjj4dw2jkpgu';
+$username = 'uac1gp3zeje8t';
+$password = 'hk8ilpc7us2e';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
